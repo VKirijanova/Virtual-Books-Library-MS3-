ec66154449589ce7bccc43598ae0a8ba1df477ab
VKirijanova <v.kirijanova@gmail.com>
2021-06-10 23:06:14 +0000

    add functionality to log in page

--- app.py
+++ app.py
@@ -29,21 +29,43 @@ def get_books():
 def register():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username")})
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("This username already exists, please try anothe")
-            return register(url_for("register"))
+            return redirect(url_for("register"))
 
         register = {
-            "username": request.form.get("usename"),
+            "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
-        session["user"] = request.form.get("username")
+
+        session["user"] = request.form.get("username").lower()
         flash("Welcome to our library!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
