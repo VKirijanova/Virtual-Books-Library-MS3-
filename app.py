6153038c3b4982748b8d73e47ac6dc55a47e7dd8
VKirijanova <v.kirijanova@gmail.com>
2021-06-11 10:37:04 +0000

    add basic books collapsible setup

--- app.py
+++ app.py
@@ -78,7 +78,18 @@ def login():
 def mypage(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("mypage.html", username=username)
+
+    if session["user"]:
+        return render_template("mypage.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
